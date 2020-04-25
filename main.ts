a6041e98558e8aeebd16663d6bc116bcd5710882
pistonstone <61121896+pistonstone@users.noreply.github.com>
2020-04-25 15:09:49 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,39 +1,47 @@
-let mySprite = sprites.create(img`
-. . . . . . e e e . . . . . . . 
-. . . . . e 3 3 3 e . . . . . . 
-. . . . . . f 3 f . . . . . . . 
-. . . . . . 3 f 3 . . . . . . . 
-. . . . . e e e e e . . . . . . 
-. . . . e e e e e e e . . . . . 
-. . . e e e e e e e e e . . . . 
-. . . e . e e e e e . e . . . . 
-. . . 3 . e e e e e . 3 . . . . 
-. . . . . e e e e e . . . . . . 
-. . . . . 8 8 8 8 8 . . . . . . 
-. . . . . 8 8 c 8 8 . . . . . . 
-. . . . . 8 8 c 8 8 . . . . . . 
-. . . . . 8 8 c 8 8 . . . . . . 
-. . . . . 8 8 c 8 8 . . . . . . 
-. . . . c c c c c c c . . . . . 
-`, SpriteKind.Player)
-let mySprite2 = sprites.create(img`
-. . . . . . 1 1 1 1 . . . . . . 
-. . . . . 1 f 1 1 f 1 . . . . . 
-. . . . . 1 f 1 1 f 1 . . . . . 
-. . . . . . 1 1 1 1 . . . . . . 
-. . . . . . 1 f f 1 . . . . . . 
-. . . . . . . 1 1 . . . . . . . 
-. . . . . 1 1 1 1 1 1 . . . . . 
-. . . . 1 1 1 1 1 1 1 1 . . . . 
-. . . . 1 . 1 1 1 1 . 1 . . . . 
-. . . . 1 . 1 1 1 1 . 1 . . . . 
-. . . . 1 . . 1 1 . . 1 . . . . 
-. . . . . . . 1 1 . . . . . . . 
-. . . . . . . 1 1 . . . . . . . 
-. . . . . . . 1 1 . . . . . . . 
-. . . . . . . 1 1 . . . . . . . 
-. . . . . 1 1 1 1 1 1 . . . . . 
-`, SpriteKind.Enemy)
+namespace SpriteKind {
+    export const ufo = SpriteKind.create()
+}
+namespace myTiles {
+    //% blockIdentity=images._tile
+    export const tile0 = img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`
+    //% blockIdentity=images._tile
+    export const tile1 = img`
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 e 7 e 7 e 
+e e 7 7 7 e e 7 7 7 7 e e 7 7 7 
+e e 7 7 7 e e e e 7 7 7 e e e e 
+e 7 e 7 e e 7 7 e e e 7 e e 7 e 
+e 7 7 e e e e e e e 7 e e 7 7 7 
+e e 7 7 e e 7 e e 7 e e e e e 7 
+e e e e e e e e 7 7 7 7 7 e e 7 
+e e e e e e e 7 7 e e e e e e e 
+e e e 7 e 7 e e e e e e e e e e 
+e e 7 7 e e e e e e e 7 7 e e e 
+e e e e e e e 7 e e e e e 7 e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+`
+}
+let projectile: Sprite = null
 scene.setBackgroundImage(img`
 f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
 f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
@@ -156,3 +164,120 @@ f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
 f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
 f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
 `)
+tiles.setTilemap(tiles.createTilemap(
+            hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001010101010101010101010101010101`,
+            img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`,
+            [myTiles.tile0,myTiles.tile1],
+            TileScale.Sixteen
+        ))
+let mySprite = sprites.create(img`
+. . . . . . e e e . . . . . . . 
+. . . . . e 3 3 3 e . . . . . . 
+. . . . . . f 3 f . . . . . . . 
+. . . . . . 3 f 3 . . . . . . . 
+. . . . . e e e e e . . . . . . 
+. . . . e e e e e e e . . . . . 
+. . . e e e e e e e e e . . . . 
+. . . e . e e e e e . e . . . . 
+. . . 3 . e e e e e . 3 . . . . 
+. . . . . e e e e e . . . . . . 
+. . . . . 8 8 8 8 8 . . . . . . 
+. . . . . 8 8 c 8 8 . . . . . . 
+. . . . . 8 8 c 8 8 . . . . . . 
+. . . . . 8 8 c 8 8 . . . . . . 
+. . . . . 8 8 c 8 8 . . . . . . 
+. . . . c c c c c c c . . . . . 
+`, SpriteKind.Player)
+controller.moveSprite(mySprite)
+let mySprite2 = sprites.create(img`
+. . . . . . 1 1 1 1 . . . . . . 
+. . . . . 1 f 1 1 f 1 . . . . . 
+. . . . . 1 f 1 1 f 1 . . . . . 
+. . . . . . 1 1 1 1 . . . . . . 
+. . . . . . 1 f f 1 . . . . . . 
+. . . . . . . 1 1 . . . . . . . 
+. . . . . 1 1 1 1 1 1 . . . . . 
+. . . . 1 1 1 1 1 1 1 1 . . . . 
+. . . . 1 . 1 1 1 1 . 1 . . . . 
+. . . . 1 . 1 1 1 1 . 1 . . . . 
+. . . . 1 . . 1 1 . . 1 . . . . 
+. . . . . . . 1 1 . . . . . . . 
+. . . . . . . 1 1 . . . . . . . 
+. . . . . . . 1 1 . . . . . . . 
+. . . . . . . 1 1 . . . . . . . 
+. . . . . 1 1 1 1 1 1 . . . . . 
+`, SpriteKind.Enemy)
+let mySprite3 = sprites.create(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . d d . . . . . . . . 
+. . . . . d d d d d . . . . . . 
+. . . d d d d d d d d d . . . . 
+. d d d d d d d d d d d d . . . 
+d d 9 d 9 d 9 d 9 d 9 d 9 d d . 
+d d d d d d d d d d d d d d d d 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+. d d d d d d d d d d d d d d . 
+. . d d d d d d d d d d d . . . 
+. . . . d d d d d d d . . . . . 
+`, SpriteKind.ufo)
+let mySprite4 = sprites.create(img`
+. . . . . 9 5 5 9 . . . . . . . 
+. . . . . 9 5 5 9 . . . . . . . 
+. . . . . 9 5 5 9 . . . . . . . 
+. . . . . 9 5 5 9 . . . . . . . 
+. . . . . 9 5 5 9 . . . . . . . 
+. . . . . 9 5 5 9 . . . . . . . 
+. . . . . 9 5 5 9 . . . . . . . 
+. . . . . 9 5 5 9 . . . . . . . 
+. . . . . 9 5 5 9 . . . . . . . 
+. . . . . 9 5 5 9 . . . . . . . 
+. . . . . 9 5 5 9 . . . . . . . 
+. . . . . 9 5 5 9 . . . . . . . 
+. . . . . 9 5 5 9 . . . . . . . 
+. . . . . 9 5 5 9 . . . . . . . 
+. . . . . 9 5 5 9 . . . . . . . 
+. . . . . 9 5 5 9 . . . . . . . 
+`, SpriteKind.Projectile)
+mySprite3.setVelocity(50, 0)
+game.onUpdateInterval(500, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+. . . . . 9 5 5 9 . . . . . . . 
+. . . . . 9 5 5 9 . . . . . . . 
+. . . . . 9 5 5 9 . . . . . . . 
+. . . . . 9 5 5 9 . . . . . . . 
+. . . . . 9 5 5 9 . . . . . . . 
+. . . . . 9 5 5 9 . . . . . . . 
+. . . . . 9 5 5 9 . . . . . . . 
+. . . . . 9 5 5 9 . . . . . . . 
+. . . . . 9 5 5 9 . . . . . . . 
+. . . . . 9 5 5 9 . . . . . . . 
+. . . . . 9 5 5 9 . . . . . . . 
+. . . . . 9 5 5 9 . . . . . . . 
+. . . . . 9 5 5 9 . . . . . . . 
+. . . . . 9 5 5 9 . . . . . . . 
+. . . . . 9 5 5 9 . . . . . . . 
+. . . . . 9 5 5 9 . . . . . . . 
+`, mySprite3, 0, 100)
+})
